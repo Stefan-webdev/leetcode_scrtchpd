85b47fc88459f66309bcd13812cbf556521d01a9
Stefan-webdev <154948992+Stefan-webdev@users.noreply.github.com>
2024-08-02 12:46:17 +0930

    loops practice js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-function lengthOfLastWord(str) {
-    // start by trimming the trailing spaces
+//function lengthOfLastWord(str) {
+    /* start by trimming the trailing spaces
     str = str.trim();
     //split the string into spaces
     const words = str.split(' ');
@@ -11,3 +11,32 @@ function lengthOfLastWord(str) {
     
 };
 console.log(lengthOfLastWord("last word"));
+
+*/ 
+
+let books = [`Happy Book`, `Sad Book`, `Jipper Book`, `Miera Book`];
+
+let oneBook = {
+    title: `Miera Book`,
+    author: `Miera Dog`,
+    publisher: `Woofing Press`
+
+}
+function loopthroughBooks(books){
+    for(let i = 0; i < books.length; i++){
+    
+        console.log(books[i])
+    }
+
+}
+
+//loopthroughBooks(books)
+
+function loopThroughObj(obj){
+    for(let key in obj){
+        console.log(key)
+        console.log(obj[key])
+    }
+}
+
+loopThroughObj(oneBook)
