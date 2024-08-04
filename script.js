f425ce5311ac35e7a14a382631f65e9ac2aaffb2
Stefan-webdev <154948992+Stefan-webdev@users.noreply.github.com>
2024-08-04 10:58:18 +0930

    House_Rbbr1

--- script.js
+++ script.js
@@ -40,3 +40,22 @@ function loopThroughObj(obj){
 }
 
 loopThroughObj(oneBook)
+
+
+//House Robber 1 Leetcode Problem. 
+function rob(nums) {
+
+    //handle edge cases
+    if (!nums.length) return 0
+    if (nums.length === 1) return nums[0];
+    if (nums.length === 2) return Math.max(nums[0], nums[1]);
+
+    for (let i=2; i<nums.length; i++) {
+        nums[i] = Math.max(nums[i-2]+nums[i], (nums[i-3] || 0) +nums[i])
+
+    }
+return Math.max(nums[nums.length-1], nums[nums.length-2]);
+
+     
+
+};
